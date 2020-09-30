b7f42c487021a31fa8161765fcfb0a9371f8c22d
Igor Ershov <criokondor@mail.ru>
2020-09-30 10:18:38 +0300

    Project calculator

---
+++ src/ru/luxoft/courses/calculator/Main.java
@@ -0,0 +1,106 @@
+package ru.luxoft.courses.calculator;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static int plus(int first, int second) {
+        return first + second;
+    }
+
+    public static int minus(int first, int second) {
+        return first - second;
+    }
+
+    public static int multiply(int first, int second) {
+        return first * second;
+    }
+
+    public static double divide(int first, int second) {
+        return ((double) first) / second;
+    }
+
+    public static int factorial(int numberOne) {
+        int res = 1;
+
+        for (int i = 1; i < numberOne; i++) {
+            res *= i;
+        }
+
+        return res;
+    }
+
+    public static double result(int firstNumber, int secondNumber, String userInput) throws InputMismatchException, ArithmeticException{
+
+        double result;
+        switch (userInput) {
+            case "+": {
+                result = plus(firstNumber, secondNumber);
+            }
+                break;
+            case "-":
+                result =  minus(firstNumber, secondNumber);
+            break;
+            case "*":
+                result = multiply(firstNumber, secondNumber);
+            break;
+            case "/": {
+                if (secondNumber == 0){
+                    throw new ArithmeticException();
+                }
+                result = divide(firstNumber, secondNumber);
+            }
+            break;
+            case "!": {
+                if (firstNumber>=20){
+                    System.out.println("Please enter number low 20");
+                    throw new InputMismatchException();
+                }
+                result = factorial(firstNumber);
+            }
+            break;
+            default: {
+                throw new InputMismatchException();
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String userInput = "";
+        int firstNumber;
+        int secondNumber;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (!"q".equals(userInput)) {
+            try {
+                System.out.println("Enter first number: ");
+                firstNumber = scanner.nextInt();
+                System.out.println("Enter second number: ");
+                secondNumber = scanner.nextInt();
+                System.out.println("Enter operation ('+', '-', '*', '/' or '!'(factorial))");
+                userInput = scanner.next();
+
+                System.out.println("You result = " +  result(firstNumber, secondNumber, userInput));
+            } catch (InputMismatchException ex) {
+                System.out.println("Wrong input data!");
+                scanner.nextLine();
+                continue;
+            }
+            catch (ArithmeticException ex) {
+                System.out.println("Wrong arithmetic operation! Divide by zero!");
+                scanner.nextLine();
+                continue;
+            }
+            System.out.println("Enter 'q' for exit or another key for continue");
+            userInput = scanner.next();
+        }
+        System.out.println("Goodbye");
+    }
+}
+
+
+
